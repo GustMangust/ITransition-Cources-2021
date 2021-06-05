de87feae33d50254bc18c5bfd5bf9a9e81e3ceac
GustMangust <gust.mangust@gmail.com>
2021-06-05 11:48:51 +0300

    feat: task 1

--- Task 1/jcs.js
+++ Task 1/jcs.js
@@ -1,30 +1,30 @@
 let wordsArray = process.argv.slice(2), tmpArray="",indexStart=0,serialString="",index = 0,counter = 0;
 wordsArray.sort();
 if (wordsArray.length === 0){
-    console.log("");
-    return;
+console.log("");
+return;
 }
 while(index<wordsArray[0].length){
-    if(counter!=wordsArray.length-1){
-        indexStart = index;
-    }
-    counter=0;
-    serialString+=wordsArray[0][index];
-    for(let i = 1;i<wordsArray.length;i++){
-        if(!wordsArray[i].includes(serialString)){
-            index = indexStart;
-            serialString ="";
-            break;
-        }
-        counter++;
-    }
-    if(counter==wordsArray.length-1 &&serialString!=""&& serialString.length>=tmpArray.length){
-        tmpArray = serialString;
-    }
-    index++;
+if(counter!=wordsArray.length-1){
+indexStart = index;
+}
+counter=0;
+serialString+=wordsArray[0][index];
+for(let i = 1;i<wordsArray.length;i++){
+if(!wordsArray[i].includes(serialString)){
+index = indexStart;
+serialString ="";
+break;
+}
+counter++;
+}
+if(counter==wordsArray.length-1 &&serialString!=""&& serialString.length>=tmpArray.length){
+tmpArray = serialString;
+}
+index++;
 }
 if(tmpArray==""){
-    console.log("");
+console.log("");
 }else{
-    console.log(tmpArray);
+console.log(tmpArray);
 }
